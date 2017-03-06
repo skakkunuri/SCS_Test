913b88712eac691cbac50af96a2a0388b36d7464
skakkunuri <srikanth.kakkunuri@gmail.com>
2017-03-07 00:46:48 +0530

    SCS first improvement checkin

---
+++ Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace SCS_Test
+{
+    partial class frmTeamAgencyAssignment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TeamComboBox = new System.Windows.Forms.ComboBox();
+            this.TeamSelectLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SelectedAgencyDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AllActiveAgencyDataGridView = new System.Windows.Forms.DataGridView();
+            this.AgencyCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AgencyName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MoveAllToSelectedButton = new System.Windows.Forms.Button();
+            this.MoveOneToAllbutton = new System.Windows.Forms.Button();
+            this.MoveOneToSelectedButton = new System.Windows.Forms.Button();
+            this.MoveSelectedToAllButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SelectedAgencyDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AllActiveAgencyDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TeamComboBox
+            // 
+            this.TeamComboBox.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TeamComboBox.FormattingEnabled = true;
+            this.TeamComboBox.Location = new System.Drawing.Point(259, 43);
+            this.TeamComboBox.Name = "TeamComboBox";
+            this.TeamComboBox.Size = new System.Drawing.Size(204, 22);
+            this.TeamComboBox.TabIndex = 1;
+            // 
+            // TeamSelectLabel
+            // 
+            this.TeamSelectLabel.AutoSize = true;
+            this.TeamSelectLabel.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TeamSelectLabel.Location = new System.Drawing.Point(171, 46);
+            this.TeamSelectLabel.Name = "TeamSelectLabel";
+            this.TeamSelectLabel.Size = new System.Drawing.Size(89, 14);
+            this.TeamSelectLabel.TabIndex = 0;
+            this.TeamSelectLabel.Text = "Select Team:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "All Active Agencies:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SelectedAgencyDataGridView);
+            this.groupBox1.Controls.Add(this.AllActiveAgencyDataGridView);
+            this.groupBox1.Controls.Add(this.MoveAllToSelectedButton);
+            this.groupBox1.Controls.Add(this.MoveOneToAllbutton);
+            this.groupBox1.Controls.Add(this.MoveOneToSelectedButton);
+            this.groupBox1.Controls.Add(this.MoveSelectedToAllButton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(645, 311);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Agencies";
+            // 
+            // SelectedAgencyDataGridView
+            // 
+            this.SelectedAgencyDataGridView.AllowUserToAddRows = false;
+            this.SelectedAgencyDataGridView.AllowUserToDeleteRows = false;
+            this.SelectedAgencyDataGridView.AllowUserToOrderColumns = true;
+            this.SelectedAgencyDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.SelectedAgencyDataGridView.Location = new System.Drawing.Point(372, 34);
+            this.SelectedAgencyDataGridView.Name = "SelectedAgencyDataGridView";
+            this.SelectedAgencyDataGridView.ReadOnly = true;
+            this.SelectedAgencyDataGridView.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.SelectedAgencyDataGridView.RowHeadersVisible = false;
+            this.SelectedAgencyDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.SelectedAgencyDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.SelectedAgencyDataGridView.Size = new System.Drawing.Size(266, 268);
+            this.SelectedAgencyDataGridView.TabIndex = 9;
+            this.SelectedAgencyDataGridView.SelectionChanged += new System.EventHandler(this.SelectedAgencyDataGridView_SelectionChanged);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Code";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 75;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Agency Name";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 250;
+            // 
+            // AllActiveAgencyDataGridView
+            // 
+            this.AllActiveAgencyDataGridView.AllowUserToAddRows = false;
+            this.AllActiveAgencyDataGridView.AllowUserToDeleteRows = false;
+            this.AllActiveAgencyDataGridView.AllowUserToOrderColumns = true;
+            this.AllActiveAgencyDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.AgencyCode,
+            this.AgencyName});
+            this.AllActiveAgencyDataGridView.Location = new System.Drawing.Point(13, 34);
+            this.AllActiveAgencyDataGridView.Name = "AllActiveAgencyDataGridView";
+            this.AllActiveAgencyDataGridView.ReadOnly = true;
+            this.AllActiveAgencyDataGridView.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.AllActiveAgencyDataGridView.RowHeadersVisible = false;
+            this.AllActiveAgencyDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.AllActiveAgencyDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.AllActiveAgencyDataGridView.Size = new System.Drawing.Size(266, 268);
+            this.AllActiveAgencyDataGridView.TabIndex = 8;
+            this.AllActiveAgencyDataGridView.SelectionChanged += new System.EventHandler(this.AllActiveAgencyDataGridView_SelectionChanged);
+            // 
+            // AgencyCode
+            // 
+            this.AgencyCode.HeaderText = "Code";
+            this.AgencyCode.Name = "AgencyCode";
+            this.AgencyCode.ReadOnly = true;
+            this.AgencyCode.Width = 75;
+            // 
+            // AgencyName
+            // 
+            this.AgencyName.HeaderText = "Agency Name";
+            this.AgencyName.Name = "AgencyName";
+            this.AgencyName.ReadOnly = true;
+            this.AgencyName.Width = 250;
+            // 
+            // MoveAllToSelectedButton
+            // 
+            this.MoveAllToSelectedButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.MoveAllToSelectedButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.MoveAllToSelectedButton.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MoveAllToSelectedButton.ForeColor = System.Drawing.Color.White;
+            this.MoveAllToSelectedButton.Location = new System.Drawing.Point(286, 109);
+            this.MoveAllToSelectedButton.Name = "MoveAllToSelectedButton";
+            this.MoveAllToSelectedButton.Size = new System.Drawing.Size(75, 23);
+            this.MoveAllToSelectedButton.TabIndex = 2;
+            this.MoveAllToSelectedButton.Text = ">>";
+            this.MoveAllToSelectedButton.UseVisualStyleBackColor = false;
+            this.MoveAllToSelectedButton.Click += new System.EventHandler(this.MoveAllToSelectedButton_Click);
+            // 
+            // MoveOneToAllbutton
+            // 
+            this.MoveOneToAllbutton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.MoveOneToAllbutton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.MoveOneToAllbutton.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MoveOneToAllbutton.ForeColor = System.Drawing.Color.White;
+            this.MoveOneToAllbutton.Location = new System.Drawing.Point(286, 167);
+            this.MoveOneToAllbutton.Name = "MoveOneToAllbutton";
+            this.MoveOneToAllbutton.Size = new System.Drawing.Size(75, 23);
+            this.MoveOneToAllbutton.TabIndex = 4;
+            this.MoveOneToAllbutton.Text = "<";
+            this.MoveOneToAllbutton.UseVisualStyleBackColor = false;
+            this.MoveOneToAllbutton.Click += new System.EventHandler(this.MoveOneToAllbutton_Click);
+            // 
+            // MoveOneToSelectedButton
+            // 
+            this.MoveOneToSelectedButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.MoveOneToSelectedButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.MoveOneToSelectedButton.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MoveOneToSelectedButton.ForeColor = System.Drawing.Color.White;
+            this.MoveOneToSelectedButton.Location = new System.Drawing.Point(286, 138);
+            this.MoveOneToSelectedButton.Name = "MoveOneToSelectedButton";
+            this.MoveOneToSelectedButton.Size = new System.Drawing.Size(75, 23);
+            this.MoveOneToSelectedButton.TabIndex = 3;
+            this.MoveOneToSelectedButton.Text = ">";
+            this.MoveOneToSelectedButton.UseVisualStyleBackColor = false;
+            this.MoveOneToSelectedButton.Click += new System.EventHandler(this.MoveOneToSelectedButton_Click);
+            // 
+            // MoveSelectedToAllButton
+            // 
+            this.MoveSelectedToAllButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.MoveSelectedToAllButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.MoveSelectedToAllButton.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MoveSelectedToAllButton.ForeColor = System.Drawing.Color.White;
+            this.MoveSelectedToAllButton.Location = new System.Drawing.Point(286, 196);
+            this.MoveSelectedToAllButton.Name = "MoveSelectedToAllButton";
+            this.MoveSelectedToAllButton.Size = new System.Drawing.Size(75, 23);
+            this.MoveSelectedToAllButton.TabIndex = 5;
+            this.MoveSelectedToAllButton.Text = "<<";
+            this.MoveSelectedToAllButton.UseVisualStyleBackColor = false;
+            this.MoveSelectedToAllButton.Click += new System.EventHandler(this.MoveSelectedToAllButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(369, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Selected Agencies for:";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(-3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(669, 28);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Team and Agency assignment";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Font = new System.Drawing.Font("Bookman Old Style", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.Location = new System.Drawing.Point(445, 391);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(99, 25);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "&Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CloseButton.Font = new System.Drawing.Font("Bookman Old Style", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseButton.Location = new System.Drawing.Point(550, 391);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(99, 25);
+            this.CloseButton.TabIndex = 4;
+            this.CloseButton.Text = "&Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // frmTeamAgencyAssignment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CloseButton;
+            this.ClientSize = new System.Drawing.Size(656, 428);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.TeamSelectLabel);
+            this.Controls.Add(this.TeamComboBox);
+            this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmTeamAgencyAssignment";
+            this.Text = "Team & Agency assignment";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SelectedAgencyDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AllActiveAgencyDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox TeamComboBox;
+        private System.Windows.Forms.Label TeamSelectLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button MoveSelectedToAllButton;
+        private System.Windows.Forms.Button MoveAllToSelectedButton;
+        private System.Windows.Forms.Button MoveOneToAllbutton;
+        private System.Windows.Forms.Button MoveOneToSelectedButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.DataGridView AllActiveAgencyDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AgencyCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AgencyName;
+        private System.Windows.Forms.DataGridView SelectedAgencyDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCS_Test
+{
+    public partial class frmTeamAgencyAssignment : Form
+    {
+        public frmTeamAgencyAssignment()
+        {
+            InitializeComponent();
+        }
+        #region "All form events"
+        /// <summary>
+        /// Form load event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        /// <summary>
+        /// When close button is clicked form will be closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// Will move all the rows from All to selected section.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MoveAllToSelectedButton_Click(object sender, EventArgs e)
+        {
+            if (AllActiveAgencyDataGridView.Rows.Count <= 0)
+                return;
+
+            AllActiveAgencyDataGridView.SelectAll();
+            MoveRowsBetweenGridViews(AllActiveAgencyDataGridView,SelectedAgencyDataGridView);
+            AllActiveAgencyDataGridView.Rows.Clear();
+            MoveAllToSelectedButton.Enabled = false;
+        }
+        /// <summary>
+        /// Move selected items from Selected to All Active grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MoveSelectedToAllButton_Click(object sender, EventArgs e)
+        {
+            if (SelectedAgencyDataGridView.Rows.Count <= 0)
+                return;
+
+            SelectedAgencyDataGridView.SelectAll();
+            //Now move all to the destination grid
+            MoveRowsBetweenGridViews(SelectedAgencyDataGridView,AllActiveAgencyDataGridView);
+            //for (int rowIndex = 0; rowIndex <= SelectedAgencyDataGridView.Rows.Count - 1; rowIndex++)
+            //{
+            //    string agencyCode = SelectedAgencyDataGridView[0, rowIndex].Value.ToString();
+            //    string agencyName = SelectedAgencyDataGridView[1, rowIndex].Value.ToString();
+            //    AllActiveAgencyDataGridView.Rows.Add(agencyCode, agencyName);
+            //}//end for
+            SelectedAgencyDataGridView.Rows.Clear();
+            MoveSelectedToAllButton.Enabled = false;
+        }
+        /// <summary>
+        /// Move selected item from All Active to SelectedAgencyDataGridView
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MoveOneToSelectedButton_Click(object sender, EventArgs e)
+        {
+            if (AllActiveAgencyDataGridView.SelectedRows.Count <= 0)
+            {
+                MessageBox.Show("No rows selected", "No rows", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            MoveRowsBetweenGridViews(AllActiveAgencyDataGridView, SelectedAgencyDataGridView,true);
+            //for (int rowIndex = 0; rowIndex <= AllActiveAgencyDataGridView.SelectedRows.Count - 1; rowIndex++)
+            //{
+            //    foreach (DataGridViewRow row in AllActiveAgencyDataGridView.SelectedRows)
+            //    {                   
+            //        // Delete from source grid
+            //        AllActiveAgencyDataGridView.Rows.RemoveAt(row.Index);
+            //    }
+            //}// end for
+        }
+        /// <summary>
+        /// This event filed when the row selection of All Active grid changed to make sure that we are disabling all the buttons properly
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AllActiveAgencyDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            MoveOneToSelectedButton.Enabled = (AllActiveAgencyDataGridView.SelectedRows.Count <= 0) ? false : true;
+            MoveAllToSelectedButton.Enabled = (AllActiveAgencyDataGridView.Rows.Count <= 0) ? false : true;
+        }
+        /// <summary>
+        /// This event filed when the row selection of SelectedAgencyDataGridView changed to make sure that we are disabling all the buttons properly
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectedAgencyDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            MoveOneToAllbutton.Enabled = (SelectedAgencyDataGridView.SelectedRows.Count <= 0) ? false : true;
+            MoveSelectedToAllButton.Enabled = (SelectedAgencyDataGridView.Rows.Count <= 0) ? false : true;
+        }
+        /// <summary>
+        /// Move selected item from SelectedAgencyDataGridView to All active grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MoveOneToAllbutton_Click(object sender, EventArgs e)
+        {
+            //if (AllActiveAgencyDataGridView.SelectedRows.Count <= 0)
+            //{
+            //    MessageBox.Show("No rows selected", "No rows", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //    return;
+            //}
+            for (int rowIndex = 0; rowIndex <= SelectedAgencyDataGridView.SelectedRows.Count - 1; rowIndex++)
+            {
+                foreach (DataGridViewRow row in SelectedAgencyDataGridView.SelectedRows)
+                {
+                    string agencyCode = row.Cells[0].Value.ToString();
+                    string agencyName = row.Cells[1].Value.ToString();
+                    // add to destination grid
+                    AllActiveAgencyDataGridView.Rows.Add(agencyCode, agencyName);
+                    // Delete from source grid
+                    SelectedAgencyDataGridView.Rows.RemoveAt(row.Index);
+                }
+            }// end
+        }
+        #endregion
+
+        #region "Private methods"
+        private void LoadData()
+        {
+            AllActiveAgencyDataGridView.Rows.Add("ADCR", "PHD USA (NY)");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG", "AMER MEDIA ADVOCACY");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG1", "AMER MEDIA ADVOCACY1");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG2", "AMER MEDIA ADVOCACY2");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG3", "AMER MEDIA ADVOCACY3");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG4", "AMER MEDIA ADVOCACY4");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG5", "AMER MEDIA ADVOCACY5");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG6", "AMER MEDIA ADVOCACY6");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG7", "AMER MEDIA ADVOCACY7");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG8", "AMER MEDIA ADVOCACY8");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG9", "AMER MEDIA ADVOCACY9");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG10", "AMER MEDIA ADVOCACY10");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG11", "AMER MEDIA ADVOCACY11");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG12", "AMER MEDIA ADVOCACY12");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG13", "AMER MEDIA ADVOCACY13");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG14", "AMER MEDIA ADVOCACY14");
+            AllActiveAgencyDataGridView.Rows.Add("AMAG15", "AMER MEDIA ADVOCACY15");            
+            this.Refresh();
+        }
+        private void MoveRowsBetweenGridViews(DataGridView sourceGridView, DataGridView destinationGridView, bool isMove = false)
+        {
+            if (sourceGridView.SelectedRows.Count <= 0)
+                return;// do not do any thing
+            for (int rowIndex = 0; rowIndex <= sourceGridView.Rows.Count - 1; rowIndex++)
+            {
+                string agencyCode = sourceGridView[0, rowIndex].Value.ToString();
+                string agencyName = sourceGridView[1, rowIndex].Value.ToString();
+                destinationGridView.Rows.Add(agencyCode, agencyName);
+                if (isMove)
+                    sourceGridView.Rows.RemoveAt(rowIndex);
+            }//end for
+        }
+        #endregion
+
+    }
+}
